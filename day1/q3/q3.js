950a522e31f77fb34107221d516ca5e53f2f59d2
AlishaAmber <alishamber11@gmail.com>
2024-04-04 01:38:50 +0500

    coding challenge

---
+++ day1/q3/q3.js
@@ -0,0 +1,7 @@
+// q3:Name Cases:Store a person'snname in variable,and then print that person's name in 
+// lowercase, uppercase and title case.
+let personalName = "Alisha";
+console.log(personalName.toLowerCase());
+console.log(personalName.toUpperCase());
+console.log(personalName.charAt(0).toUpperCase() + personalName.slice(1).toLowerCase());
+export {};
---
+++ day1/q3/q3.ts
@@ -0,0 +1,10 @@
+// q3:Name Cases:Store a person'snname in variable,and then print that person's name in 
+// lowercase, uppercase and title case.
+
+let personalName: string= "Alisha";
+
+console.log(personalName.toLowerCase());
+
+console.log(personalName.toUpperCase());
+
+console.log(personalName.charAt(0).toUpperCase() + personalName.slice(1).toLowerCase());
